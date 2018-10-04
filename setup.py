014b8273ba3d057ca1a6818601815313121b9080
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-10-05 00:16:38 +0100

    package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="benchmark",
+    version="0.1",
+    author="Pedro F. Ferreira",
+    description="Package for comparing scRNA-seq models.",
+    packages=find_packages(),
+)
