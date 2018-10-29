e38b03040a99b6c2e1f59ab73850a0153e56a391
pedrofale <pedro.miguel.ferreira.pf@gmail.com>
2018-10-29 11:25:19 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="benchmark",
     version="0.1",
@@ -7,4 +10,5 @@ setup(
     description="Package for comparing scRNA-seq models.",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
 )
